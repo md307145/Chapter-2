28db1bd119b85cc533aa1857faee1012ed6c00f1
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-12 13:19:58 -0500

    idk

---
+++ src/ArithmeticDemo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner ;
+public class ArithmeticDemo {
+    public static void main (String [] args){
+        int firstNumber;
+        int secondNumber;
+        int sum;
+        int difference;
+        int average;
+
+        Scanner input = new Scanner(System. in);
+        System.out.println("Please enter an interger >> ");
+        firstNumber = input.nextInt();
+        System.out.println("Please enter an interger >> ");
+        secondNumber = input.nextInt();
+
+        sum = firstNumber + secondNumber;
+        difference =firstNumber - secondNumber;
+        average = sum / 2;
+
+        System.out.println(firstNumber +" + " + secondNumber + "is" + sum);
+        System.out.println(firstNumber +" - " + secondNumber + "is" + difference);
+        System.out.println(" The average of" + firstNumber + "and"  + secondNumber + "is" + average);
+    }
+}

8c884bde0171c78708196918d6e33c40204bc83d
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-12 12:29:40 -0500

    idk

---
+++ src/DataDemo.java
@@ -0,0 +1,10 @@
+public class DataDemo
+{
+ public static void main (String [] args) {
+  int aWholeNumber = 315;
+  System.out.println("The number is ");
+  System.out.println("aWholeNumber");
+
+ }
+
+}

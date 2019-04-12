3f85ed029a9a50512c934ebd88937c871fd3c18b
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-12 12:50:30 -0500

    idk

---
+++ src/CharDemo.java
@@ -0,0 +1,9 @@
+public class CharDemo {
+    public static void main(String [] args)
+    {
+        char initial ='A';
+        System.out.println(initial);
+        System.out.println("\t\"abc\\def\\bgh\\n\njkl");
+        
+    }
+}

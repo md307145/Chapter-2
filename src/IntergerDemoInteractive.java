2e4a80926608a24b4d2d156824b89b3af7c1a49b
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-15 12:24:06 -0500

    l;

--- src/IntergerDemoInteractive.java
+++ src/IntergerDemoInteractive.java
@@ -8,14 +8,14 @@ public class IntergerDemoInteractive
      short aShort = 12;
      long aLong = 12;
         System.out.println("The int is" + anInt);
-
+        Scanner input = new Scanner(System. in);
         System.out.println("The byte is" + aByte);
         aByte  = input.nextByte();
         System.out.println("The short is"+ aShort);
         aShort = input.nextShort();
         System.out.println("The long is"+ aLong);
         aLong = input.nextLong();
-        Scanner input = new Scanner(System. in);
+
         System.out.println("pleaseenter an interger >>");
         anInt = input.nextInt ();
     }

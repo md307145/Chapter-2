25b8b7a1bba12783d0d48b39ace05a467d68efbc
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-04-18 13:16:35 -0500

    4/18/2019

--- src/QuartsToGallonsInteractive.java
+++ src/QuartsToGallonsInteractive.java
@@ -5,15 +5,24 @@ public class QuartsToGallonsInteractive
     public static void main(String [] args)
     {
         int numberOfQuartsInputed;
-        final int NUM_QUARTS_IN_GALLON= 4;
-        int remainderOfQuoets;
-        int numberOfGallons;
+
         Scanner keyboard= new Scanner(System.in);
         System.out.println("Please enter the number of quorts needeed to do this job>>>");
         numberOfQuartsInputed =keyboard.nextInt();
 
+        displayMath( numberOfQuartsInputed);
+
+
+    }
+    public static void displayMath( int numberOfQuartsInputed) {
+        final int NUM_QUARTS_IN_GALLON= 4;
+        int remainderOfQuoets;
+        int numberOfGallons;
         numberOfGallons= numberOfQuartsInputed / NUM_QUARTS_IN_GALLON;
         remainderOfQuoets= numberOfQuartsInputed % NUM_QUARTS_IN_GALLON;
-        System.out.println("The number of gallons ordered is " + numberOfGallons + " and the number of quarts remaining is4 " + remainderOfQuoets);
+        System.out.println("The number of gallons ordered is " + numberOfGallons + " and the number of quarts \n" +
+                "remaining" +
+                " is " + remainderOfQuoets);
     }
+
 }
